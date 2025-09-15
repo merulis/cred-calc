b35b9ef4871803e2dfe7b65d08c3e31b4758998f
merulis <merulis@yandex.ru>
2025-09-15 19:22:17 +0300

    lint

--- src/settlementCalculator.py
+++ src/settlementCalculator.py
@@ -8,7 +8,6 @@ from src.struct.matchedPayment import MatchedPayment
 
 
 class SettlementCalculator:
-
     @staticmethod
     def make_settlement(
         debits: deque[Debit],
@@ -17,19 +16,17 @@ class SettlementCalculator:
     ) -> list:
         result = []
         for credit in credits:
-            
             while credit > 0 and debits:
                 debit = debits[0]
                 credit_paid = credit.amount
-                
+
                 SettlementCalculator._match_payment(debit, credit)
 
                 due_date = debit.date + timedelta(days=deadline_days)
-                overdue_days = max((credit.date - due_date).days, 0)       
+                overdue_days = max((credit.date - due_date).days, 0)
 
                 penalties = SettlementCalculator._counting_of_penalties(
-                    credit=credit_paid,
-                    overdue_days=overdue_days
+                    credit=credit_paid, overdue_days=overdue_days
                 )
 
                 result.append(
@@ -42,7 +39,7 @@ class SettlementCalculator:
                         overdue_days=overdue_days,
                         penalty_base=penalties[0],
                         penalty_additional=penalties[1],
-                        penalty_percent=penalties[2]
+                        penalty_percent=penalties[2],
                     )
                 )
 
@@ -56,35 +53,36 @@ class SettlementCalculator:
         credit.amount -= matched_amount
 
     @staticmethod
-    def _counting_of_penalties(
-            credit: Credit, 
-            overdue_days: int
-    ):
+    def _counting_of_penalties(credit: Credit, overdue_days: int):
         return (
-            SettlementCalculator._calculate_base_penalty(credit, overdue_days),
-            SettlementCalculator._calculate_additionaly_penalty(credit),
-            SettlementCalculator._calculate_percent_penalty(credit, overdue_days),
+            SettlementCalculator._calculate_base_penalty(
+                credit,
+                overdue_days,
+            ),
+            SettlementCalculator._calculate_additionaly_penalty(
+                credit,
+            ),
+            SettlementCalculator._calculate_percent_penalty(
+                credit,
+                overdue_days,
+            ),
         )
 
     @staticmethod
     def _calculate_base_penalty(
-            credit_amount: Decimal, 
-            overdue_days: int, 
-            penalty_rate=0.5
+        credit_amount: Decimal, overdue_days: int, penalty_rate=0.5
     ) -> Decimal:
-        return credit_amount * (penalty_rate / 100) * overdue_days 
+        return credit_amount * (penalty_rate / 100) * overdue_days
 
     @staticmethod
-    def _calculate_additionaly_penalty( 
-            amount: Decimal, 
-            penalty_rate=90
+    def _calculate_additionaly_penalty(
+        amount: Decimal,
+        penalty_rate=90,
     ) -> Decimal:
         return amount * (penalty_rate / 100)
 
     @staticmethod
     def _calculate_percent_penalty(
-            amount: Decimal, 
-            overdue_days: int, 
-            bank_rate: int = 16
+        amount: Decimal, overdue_days: int, bank_rate: int = 16
     ) -> Decimal:
         return amount * (bank_rate / 100) * overdue_days / 365
