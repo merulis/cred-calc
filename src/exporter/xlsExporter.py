e9e35d8fe7e9e7e8c862ed306f126febdf99626f
merulis <merulis@yandex.ru>
2025-09-22 22:27:55 +0300

    add exporters

--- src/exporter/xlsExporter.py
+++ src/exporter/xlsExporter.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+from src.models import MatchedPayment
+
 class XlsExporter:
     def export_to_xls(result: list, filepath: str):
-        df = pd.DataFrame([r.to_list() for r in result], columns=)
+        df = pd.DataFrame([r.to_list() for r in result], columns=MatchedPayment.headers())
+        df.to_excel(filepath, index=False)
